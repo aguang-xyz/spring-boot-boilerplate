47f4cf5a072f5d26021dc090305e68a1df213b3f
aguang <aguang@aguang.xyz>
2019-07-02 20:31:50 +0800

    Initialize spring boot with docker support.

---
+++ src/main/java/xyz/aguang/boilerplate/BoilerplateApp.java
@@ -0,0 +1,13 @@
+package xyz.aguang.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateApp {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(BoilerplateApp.class, args);
+  }
+}
